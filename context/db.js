d5abdc8d001b7bbb9efee121e81b25def06ba80b
zemla4ok <dima-vital@live.com>
2018-04-07 10:43:03 +0300

    add database

---
+++ context/db.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = (Sequelize, config) => {
+  const options = { host: config.db.host,
+    dialect: config.db.dialect,
+    dialectOptions: { ssl: true } };
+
+  const sequelize = new Sequelize(config.db.name,
+    config.db.user, config.db.password, options);
+  const Rand = require('../models/rand')
+    (Sequelize, sequelize);
+
+  return { rand: Rand, sequelize: sequelize };
+};
--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 'use strict';
 
 const express = require('express');
-const app = express();
+const Sequelize = require('sequelize');
 
 const port = process.env.PORT || 3000;
+const config = require('./config');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const dbcontext = require('./context/db')
+  (Sequelize, config);
+
+const app = express();
+
+app.get('/', (req, res) =>{
+  dbcontext.rand
+  .findAll({ raw: true })
+  .then((rands) => res.json(rands));
+});
+app.get('/generate', (req, res) => {
+  dbcontext.rand
+    .create({
+      value: Math.random()
+    })
+    .then((rand) => res.json(rand));
 });
 
-app.listen(port, function () {
-  console.log(`Server running on ${port}`);
-});
-      
+dbcontext.sequelize
+  .sync()
+  .then(() => {
+    app.listen(port, () => {
+        console.log(`Running on ${port} port`);
+    });
+  });
